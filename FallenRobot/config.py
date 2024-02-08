47fc18a47887d35b9fe8937751e297fab721a66f
Wannabeded <159153057+Wannabeded@users.noreply.github.com>
2024-02-08 09:21:47 +0530

    Update config.py
    Signed-off-by: Wannabeded <159153057+Wannabeded@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,7 +14,7 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph/file/97c2f98f20c664a4a5ac5.jpg"
+    START_IMG = "https://telegra.ph/file/18ffb518121bca44a4c1e.png"
 
     SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
 
